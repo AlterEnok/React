57e7c1087169deeb48bdd94060557c92da3bc037
Edem Kestan <ed.kestan@gmail.com>
2024-03-25 00:12:18 +0200

    upd

---
+++ task1/index.js
@@ -0,0 +1,8 @@
+const rootElement = document.querySelector('#root');
+
+const greetingElem = React.createElement(
+    'div',
+    { class: 'greeting' },
+    'Hello, React!'
+);
+ReactDOM.render(greetingElem, rootElement);
