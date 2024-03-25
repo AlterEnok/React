2e4c4648041aa183319403ea4f88a8daf4633ac2
Edem Kestan <ed.kestan@gmail.com>
2024-03-25 02:54:24 +0200

    upd

--- lesson1/task3/webpack.config.js
+++ lesson1/task3/webpack.config.js
@@ -1,22 +1,49 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    entry: './src/static/index.js',
+    output: {
+        path: path.resolve(__dirname, '..', 'review_build'),
+        filename: '[name].bundle.js',
+        publicPath: '',
+    },
     module: {
         rules: [
             {
-                test: /.js$/,
-                use: ["babel-loader"]
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             },
-
             {
-                test: /.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ]
+            template: './src/static/index.html',
+            filename: './index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/static', to: 'static' },
+            ],
+        }),
+    ],
+    devServer: {
+        contentBase: './review_build',
+        hot: true,
+    },
 };
