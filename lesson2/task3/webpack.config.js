f53829dbff2f7462fc063a5d05707f44bbd1f903
Edem Kestan <ed.kestan@gmail.com>
2024-03-29 00:37:36 +0200

    task3

---
+++ lesson2/task3/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/index.css', to: 'styles.css' },
+            ],
+        }),
+    ],
+    devServer: {
+        contentBase: './review_build',
+        hot: true,
+    },
+};
