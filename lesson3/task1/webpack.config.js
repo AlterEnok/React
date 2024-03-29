f7b02d04b8b4b97c36e8a171f3a6e3090c17598c
Edem Kestan <ed.kestan@gmail.com>
2024-03-29 21:45:43 +0200

    task1

---
+++ lesson3/task1/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/index.scss', to: 'styles.css' },
+                { from: './src/search.scss', to: 'search.scss' },
+            ],
+        }),
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+};
