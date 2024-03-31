a9db46f9ec64c5dad3cc1d05cc37b8dfaf3b9d1d
Edem Kestan <ed.kestan@gmail.com>
2024-03-31 20:27:54 +0300

    upd

--- lesson3/Greeting/src/index.jsx
+++ lesson3/Greeting/src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Greeting from './Greeting';
 
+const rootElement = document.getElementById('root');
+
 ReactDOM.render(
     <Greeting firstName="John" lastName="Doe" birthDate={new Date(2005, 0, 1)} />,
-    document.getElementById('root')
+    rootElement
 );
