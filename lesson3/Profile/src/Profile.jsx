8f46cdb9a571c06d45fccbb6b4a66ac661736766
Edem Kestan <ed.kestan@gmail.com>
2024-04-01 14:05:07 +0300

    task

---
+++ lesson3/Profile/src/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import moment from 'moment';
+
+function Profile({ userData }) {
+    const { firstName, lastName, birthDate, birthPlace } = userData;
+    const formattedBirthDate = moment(birthDate).format('DD MM YY');
+
+    return (
+        <div className="profile">
+            <div className="profile__name">{firstName} {lastName}</div>
+            <div className="profile__birth">Was born {formattedBirthDate} in {birthPlace}</div>
+        </div>
+    );
+}
+
+export default Profile;
