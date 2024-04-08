8b29852e87b17af90a947a9cc50b06c25c6eac08
Edem Kestan <ed.kestan@gmail.com>
2024-04-09 00:49:54 +0300

    task

--- lesson6/task1/src/Auth.jsx
+++ lesson6/task1/src/Auth.jsx
@@ -1,50 +1,41 @@
 import React from 'react';
 import Greeting from './Greeting';
-
-
+import Login from './Login';
+import Logout from './Logout';
 
 class Auth extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             isLoggedIn: false,
-        }
+        };
     }
 
     handleLogin = () => {
         this.setState({
             isLoggedIn: true,
         });
-    }
+    };
 
     handleLogout = () => {
         this.setState({
             isLoggedIn: false,
         });
-    }
+    };
 
     render() {
-        let button;
-
-        if (this.state.isLoggedIn) {
-            button = <button onClick={this.handleLogout}>Logout</button>;
-        } else {
-            button = <button onClick={this.handleLogin}>Login</button>;
-        }
+        const { isLoggedIn } = this.state;
         return (
             <div className="panel">
-                <Greeting isLoggedIn={this.state.isLoggedIn} />
-                <div>
-                    <div>{button}</div>
-                </div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {isLoggedIn ? (
+                    <Logout onLogout={this.handleLogout} />
+                ) : (
+                    <Login onLogin={this.handleLogin} />
+                )}
             </div>
         );
     }
-
-
 }
-
-
 
 export default Auth;
