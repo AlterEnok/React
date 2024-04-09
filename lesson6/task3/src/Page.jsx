bacd7aeb9a989ddbc60db2dd819a3c8885aba71f
Edem Kestan <ed.kestan@gmail.com>
2024-04-09 23:59:19 +0300

    task

---
+++ lesson6/task3/src/Page.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Info from './Info';
+
+class Page extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: null
+        };
+    }
+
+    setInfo = info => {
+        this.setState({
+            info
+        });
+    };
+
+    render() {
+        return (
+            <div className="page">
+                <Info info={this.state.info} />
+                <div className="actions">
+                    <button className="btn" onClick={() => this.setInfo('IPhone 13 - Price is 500$. Available in 2 colors')} >Info 1</button>
+                    <button className="btn" onClick={() => this.setInfo('Price is 650$. Not available')}>Info 2</button>
+                    <button className="btn" onClick={() => this.setInfo('')}>Clear</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Page;
