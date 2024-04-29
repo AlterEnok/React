9ff8d2e1747fc26899355dc348c7d360625e7a1f
Edem Kestan <ed.kestan@gmail.com>
2024-04-29 17:31:56 +0300

    task

---
+++ lesson8/task1/src/Life.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Life extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('constructor: good place to create state');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount: API calls, subscriptions');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate: decide to render or not to render');
+        return true;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate: some updates based on new props');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
+    }
+
+
+    render() {
+        return <div className='number'>{this.props.number}</div>
+    }
+}
+
+export default Life;
+
