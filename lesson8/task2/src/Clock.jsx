23f3f526c0b4f352bac2b61728027742347723a5
Edem Kestan <ed.kestan@gmail.com>
2024-05-01 16:31:04 +0300

    task

--- lesson8/task2/src/Clock.jsx
+++ lesson8/task2/src/Clock.jsx
@@ -4,14 +4,14 @@ class Clock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: this.getLocalTime(props.offset),
+            date: this.calculateTime(),
         };
     }
 
     componentDidMount() {
         this.interval = setInterval(() => {
             this.setState({
-                date: this.getLocalTime(this.props.offset),
+                date: this.calculateTime(),
             });
         }, 1000);
     }
@@ -20,18 +20,34 @@ class Clock extends Component {
         clearInterval(this.interval);
     }
 
-    getLocalTime(offset) {
-        const now = new Date();
-        const utc = now.getTime() + now.getTimezoneOffset() * 60000;
-        const localTime = new Date(utc + 3600000 * offset);
-        return localTime.toLocaleTimeString();
+    calculateTime() {
+        const { offset } = this.props;
+        const currentDate = new Date();
+        const utc = currentDate.getTime() + (currentDate.getTimezoneOffset() * 60000);
+        const newDate = new Date(utc + (3600000 * offset));
+
+        return newDate;
+    }
+
+    formatTime(date) {
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        const formattedHours = hours % 12 || 12;
+
+        return `${this.formatDigit(formattedHours)}:${this.formatDigit(minutes)}:${this.formatDigit(seconds)} ${ampm}`;
+    }
+
+    formatDigit(digit) {
+        return digit < 10 ? `0${digit}` : digit;
     }
 
     render() {
         return (
-            <div className="clock">
-                <div className="clock__location">{this.props.location}</div>
-                <div className="clock__time">{this.state.date}</div>
+            <div className='clock'>
+                <div className='clock__location'>{this.props.location}</div>
+                <div className='clock__time'>{this.formatTime(this.state.date)}</div>
             </div>
         );
     }
