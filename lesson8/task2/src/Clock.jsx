c199b197f2369144602492a2df581b0b9b95be5f
Edem Kestan <ed.kestan@gmail.com>
2024-05-01 13:18:24 +0300

    upd

--- lesson8/task2/src/Clock.jsx
+++ lesson8/task2/src/Clock.jsx
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
 
-
 class Clock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: new Date(),
+            date: this.calculateTime(props.offset),
         };
     }
 
-    ComponentDidMount() {
+    componentDidMount() {
         this.interval = setInterval(() => {
             this.setState({
-                date: new Date(),
+                date: this.calculateTime(this.props.offset),
             });
         }, 1000);
     }
 
-    ComponentWillUnmount() {
+    componentWillUnmount() {
         clearInterval(this.interval);
     }
+
+    calculateTime(offset) {
+        const currentDate = new Date();
+        const utc = currentDate.getTime() + (currentDate.getTimezoneOffset() * 60000);
+        const newDate = new Date(utc + (3600000 * offset));
+        return newDate;
+    }
+
     render() {
         return (
             <div className='clock'>
                 <div className='clock__location'>{this.props.location}</div>
                 <div className='clock__time'>{this.state.date.toLocaleTimeString()}</div>
             </div>
-        )
+        );
     }
 }
 
