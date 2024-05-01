04b6fc9612d18c5be371aa515212bec531dc272d
Edem Kestan <ed.kestan@gmail.com>
2024-05-01 16:13:20 +0300

    task

--- lesson8/task2/src/Clock.jsx
+++ lesson8/task2/src/Clock.jsx
@@ -4,14 +4,14 @@ class Clock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: this.calculateTime(props.offset),
+            date: this.getLocalTime(props.offset),
         };
     }
 
     componentDidMount() {
         this.interval = setInterval(() => {
             this.setState({
-                date: this.calculateTime(this.props.offset),
+                date: this.getLocalTime(this.props.offset),
             });
         }, 1000);
     }
@@ -20,18 +20,18 @@ class Clock extends Component {
         clearInterval(this.interval);
     }
 
-    calculateTime(offset) {
-        const currentDate = new Date();
-        const utc = currentDate.getTime() + (currentDate.getTimezoneOffset() * 60000);
-        const newDate = new Date(utc + (3600000 * offset));
-        return newDate;
+    getLocalTime(offset) {
+        const now = new Date();
+        const utc = now.getTime() + now.getTimezoneOffset() * 60000;
+        const localTime = new Date(utc + 3600000 * offset);
+        return localTime.toLocaleTimeString();
     }
 
     render() {
         return (
-            <div className='clock'>
-                <div className='clock__location'>{this.props.location}</div>
-                <div className='clock__time'>{this.state.date.toLocaleTimeString()}</div>
+            <div className="clock">
+                <div className="clock__location">{this.props.location}</div>
+                <div className="clock__time">{this.state.date}</div>
             </div>
         );
     }
